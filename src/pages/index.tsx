ba671b37835e2ba683b5fbb149634d02ef6828e8
DiegoCapella <diegocapelladev@gmail.com>
2022-06-21 15:18:24 -0300

    refactor: index on pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-const Index = () => {
-  return <h1>Hello World!</h1>
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <h1>Hello world!</h1>
 }
 
-export default Index
+export default Home
