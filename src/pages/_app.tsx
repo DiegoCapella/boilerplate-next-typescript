3d3216919fc6a6ff9867050bb06ea717c1a89733
DiegoCapella <diegocapelladev@gmail.com>
2022-06-21 15:03:06 -0300

    "chore: config styled-components and theme provider"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from 'styles/globals'
+import { theme } from 'styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Next Boilerplate</title>
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project starter to work with React, NextJS, TypeScript and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
